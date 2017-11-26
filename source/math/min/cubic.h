51134db5d8e79a5d98bba0458d408f3dc1af5827
Aaron-SP <springstrohaa@gmail.com>
2017-11-26 18:52:00 -0500

    MGL: Added cubic derivative interpolations

--- source/math/min/cubic.h
+++ source/math/min/cubic.h
@@ -28,6 +28,7 @@ class bezier
     vec<T> _p3;
 
   public:
+    bezier() {}
     bezier(const vec<T> &p0, const vec<T> &p1, const vec<T> &p2, const vec<T> &p3)
         : _p0(p0), _p1(p1), _p2(p2), _p3(p3) {}
 
@@ -50,6 +51,55 @@ class bezier
         // return interpolated point on bezier curve
         return (_p0 * b0) + (_p1 * b1) + (_p2 * b2) + (_p3 * b3);
     }
+    const min::vec3<float> &begin() const
+    {
+        return _p0;
+    }
+    const min::vec3<float> &end() const
+    {
+        return _p3;
+    }
+};
+
+template <typename T, template <typename> class vec>
+class bezier_deriv
+{
+  private:
+    vec<T> _p0;
+    vec<T> _p1;
+    vec<T> _p2;
+    vec<T> _p3;
+
+  public:
+    bezier_deriv() {}
+    bezier_deriv(const vec<T> &p0, const vec<T> &p1, const vec<T> &p2, const vec<T> &p3)
+        : _p0(p0), _p1(p1), _p2(p2), _p3(p3) {}
+
+    // Assumes t is between domain [0.0, 1.0]
+    vec<T> interpolate(const T t) const
+    {
+        // Precalculate time constants
+        const T t2 = t * t;
+        const T u = 1.0 - t;
+        const T u2 = u * u;
+
+        // Calculate blending functions
+        const T b0 = -3.0 * u2;
+        const T b1 = 3.0 - (12.0 * t) + (9.0 * t2);
+        const T b2 = (6.0 * t) - (9.0 * t2);
+        const T b3 = 3.0 * t2;
+
+        // return interpolated point on bezier_deriv curve
+        return (_p0 * b0) + (_p1 * b1) + (_p2 * b2) + (_p3 * b3);
+    }
+    const min::vec3<float> &begin() const
+    {
+        return _p0;
+    }
+    const min::vec3<float> &end() const
+    {
+        return _p3;
+    }
 };
 
 template <typename T, template <typename> class vec>
@@ -62,6 +112,7 @@ class bspline
     vec<T> _p3;
 
   public:
+    bspline() {}
     // Points p1 & p2 define the curve endpoints; p0 and p3 are control points
     bspline(const vec<T> &p0, const vec<T> &p1, const vec<T> &p2, const vec<T> &p3)
         : _p0(p0), _p1(p1), _p2(p2), _p3(p3) {}
@@ -77,12 +128,62 @@ class bspline
 
         // Calculate blending functions
         const T b0 = u3;
-        const T b1 = 4.0 - 6.0 * t2 + 3.0 * t3;
-        const T b2 = 1.0 + 3.0 * t + 3.0 * t2 - 3.0 * t3;
+        const T b1 = 4.0 - (6.0 * t2) + (3.0 * t3);
+        const T b2 = 1.0 + (3.0 * t) + (3.0 * t2) - (3.0 * t3);
         const T b3 = t3;
 
         // return interpolated point on bspline curve
         return ((_p0 * b0) + (_p1 * b1) + (_p2 * b2) + (_p3 * b3)) * 0.166667;
+    }
+    const min::vec3<float> &begin() const
+    {
+        return _p1;
+    }
+    const min::vec3<float> &end() const
+    {
+        return _p2;
+    }
+};
+
+template <typename T, template <typename> class vec>
+class bspline_deriv
+{
+  private:
+    vec<T> _p0;
+    vec<T> _p1;
+    vec<T> _p2;
+    vec<T> _p3;
+
+  public:
+    bspline_deriv() {}
+    // Points p1 & p2 define the curve endpoints; p0 and p3 are control points
+    bspline_deriv(const vec<T> &p0, const vec<T> &p1, const vec<T> &p2, const vec<T> &p3)
+        : _p0(p0), _p1(p1), _p2(p2), _p3(p3) {}
+
+    // Assumes t is between domain [0.0, 1.0]
+    vec<T> interpolate(const T t) const
+    {
+        // Precalculate time constants
+        const T t2 = t * t;
+        const T u = 1.0 - t;
+        const T u2 = u * u;
+
+        // Calculate blending functions
+        const T b0 = -0.5 * u2;
+        const T b1 = (-2.0 * t) + (1.5 * t2);
+        const T b2 = 0.5 + t - (1.5 * t2);
+        const T b3 = 0.5 * t2;
+
+        // return interpolated point on bspline_deriv curve
+        return ((_p0 * b0) + (_p1 * b1) + (_p2 * b2) + (_p3 * b3));
+    }
+    const min::vec3<float> &begin() const
+    {
+        return _p1;
+    }
+    const min::vec3<float> &end() const
+    {
+        return _p2;
     }
 };
 
@@ -96,6 +197,7 @@ class hermite
     vec<T> _t1;
 
   public:
+    hermite() {}
     // Points p0 & p1 and tangents at those points t0, t1
     hermite(const vec<T> &p0, const vec<T> &p1, const vec<T> &t0, const vec<T> &t1)
         : _p0(p0), _p1(p1), _t0(t0), _t1(t1) {}
@@ -110,13 +212,62 @@ class hermite
         const T u2 = u * u;
 
         // Calculate blending functions
-        const T b0 = 1.0 - 3.0 * t2 + 2.0 * t3;
-        const T b1 = t2 * (3.0 - 2.0 * t);
+        const T b0 = 1.0 - (3.0 * t2) + (2.0 * t3);
+        const T b1 = t2 * (3.0 - (2.0 * t));
         const T b2 = t * u2;
         const T b3 = t2 * u;
 
         // return interpolated point on hermite curve
         return (_p0 * b0) + (_p1 * b1) + (_t0 * b2) + (_t1 * b3);
+    }
+    const min::vec3<float> &begin() const
+    {
+        return _p0;
+    }
+    const min::vec3<float> &end() const
+    {
+        return _p1;
+    }
+};
+
+template <typename T, template <typename> class vec>
+class hermite_deriv
+{
+  private:
+    vec<T> _p0;
+    vec<T> _p1;
+    vec<T> _t0;
+    vec<T> _t1;
+
+  public:
+    hermite_deriv() {}
+    // Points p0 & p1 and tangents at those points t0, t1
+    hermite_deriv(const vec<T> &p0, const vec<T> &p1, const vec<T> &t0, const vec<T> &t1)
+        : _p0(p0), _p1(p1), _t0(t0), _t1(t1) {}
+
+    // Assumes t is between domain [0.0, 1.0]
+    vec<T> interpolate(const T t) const
+    {
+        // Precalculate time constants
+        const T t2 = t * t;
+        const T u = 1.0 - t;
+
+        // Calculate blending functions
+        const T b0 = (-6.0 * t) + (6.0 * t2);
+        const T b1 = 6.0 * t * u;
+        const T b2 = 1.0 - (4.0 * t) + (3.0 * t2);
+        const T b3 = (2.0 * t) - (3.0 * t2);
+
+        // return interpolated point on hermite_deriv curve
+        return (_p0 * b0) + (_p1 * b1) + (_t0 * b2) + (_t1 * b3);
+    }
+    const min::vec3<float> &begin() const
+    {
+        return _p0;
+    }
+    const min::vec3<float> &end() const
+    {
+        return _p1;
     }
 };
 }
